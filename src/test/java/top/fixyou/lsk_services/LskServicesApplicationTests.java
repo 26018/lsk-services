39bbee205bc0083fe1b90dab0138ab1e1f9d1aec
26018 <2033471349@qq.com>
2023-02-27 11:22:50 +0800

    init_project

---
+++ src/test/java/top/fixyou/lsk_services/LskServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package top.fixyou.lsk_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LskServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
